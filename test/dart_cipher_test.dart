a4f460c51649cfcb2b197ab80c8c5698019c463c
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-04-07 11:02:06 +0800

    update tests

--- test/dart_cipher_test.dart
+++ test/dart_cipher_test.dart
@@ -6,16 +6,20 @@ void main() {
   group('cipher tests =>', () {
     test('encrypt and decrypt should return the same string.', () {
       const String input = 'secret message';
-      final String encrypted = Cipher.encrypt(input);
-      final String decrypted = Cipher.decrypt(encrypted);
+      const String key = 'secret key';
+
+      final String encrypted = Cipher.encrypt(input, key);
+      final String decrypted = Cipher.decrypt(encrypted, key);
 
       expect(decrypted, equals(input));
     });
 
     test('encrypt and decrypt should work with empty strings.', () {
       const String input = '';
-      final String encrypted = Cipher.encrypt(input);
-      final String decrypted = Cipher.decrypt(encrypted);
+      const String key = 'secret key';
+
+      final String encrypted = Cipher.encrypt(input, key);
+      final String decrypted = Cipher.decrypt(encrypted, key);
 
       expect(decrypted, equals(input));
     });
@@ -24,13 +28,16 @@ void main() {
         'encrypt and decrypt should produce different results with different keys.',
         () {
       const String input = 'secret message';
-      final String encrypted1 = Cipher.encrypt(input);
-      final String encrypted2 = Cipher.encrypt(input);
+      const String key1 = 'secret key 1';
+      const String key2 = 'secret key 2';
+
+      final String encrypted1 = Cipher.encrypt(input, key1);
+      final String encrypted2 = Cipher.encrypt(input, key2);
 
       expect(encrypted1, isNot(equals(encrypted2)));
 
-      final String decrypted1 = Cipher.decrypt(encrypted1);
-      final String decrypted2 = Cipher.decrypt(encrypted2);
+      final String decrypted1 = Cipher.decrypt(encrypted1, key1);
+      final String decrypted2 = Cipher.decrypt(encrypted2, key2);
 
       expect(decrypted1, equals(input));
       expect(decrypted2, equals(input));
